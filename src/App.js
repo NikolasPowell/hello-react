a3fd1449920e406af4060eb75262c50b67760cc8
Nikolas Powell <powel253@msu.edu>
2023-04-03 15:09:38 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 const toptitanarmor = [
-  {id: 1, name: 'Synthoceps', description: 'Increased melee stats when surrounded by enemies', melee: true },
-  {id: 2, name: 'Heart of Inmost Light', description: 'Increased regeneration of abilities by 50%', melee: false },
-  {id: 3, name: 'Loreley Splendor Helm', description: 'When below critical health, a solar flame will spawn to start regeneration', melee: false }
+  {id: 1, name: 'Synthoceps', description: 'Increased melee stats when surrounded by enemies', element: true },
+  {id: 2, name: 'Heart of Inmost Light', description: 'Increased regeneration of abilities by 50%', element: false },
+  {id: 3, name: 'Loreley Splendor Helm', description: 'When below critical health, a solar flame will spawn to start regeneration', element: true }
 ];
 
 function TopList() {
   const threelist = toptitanarmor.map(titan =>
     <div>
-    <h3
-    key={titan.id}
-    style={{
-      color: titan.melee ? 'blue' : 'red'
-    }}
-    >
-      <h2>{titan.name}</h2>
-    </h3>
-      <p>{titan.description}</p>
+        <h3
+          key={titan.id}
+          style={{
+            color: titan.element ? 'red' : 'lightblue'
+          }}
+        >
+          <h2>{titan.name}</h2>
+        </h3>
+          <p>{titan.description}</p>
     </div>
   );
   return(
@@ -54,14 +54,20 @@ function Information() {
   );
 }
 
+function Goodchoice() {
+  alert("Thats a Great Choice! Some people will pick the other option and i'm glad you didnt. So yay!");
+}
+
+function Badchoice() {
+  alert("Thats not really a great choice...Playing the game is loads of fun! I say take a look into it and see what you can do!")
+}
+
 function Decision() {
   return(
     <div>
       <h2>Do you like or dont like Destiny 2?</h2>
-      <view>
-        <button>Like</button>
-      </view>
-      <button>Dislike</button>
+        <button className='like' onClick={Goodchoice}>Like</button>
+        <button className='dislike' onClick={Badchoice}>Dislike</button>
     </div>
   );
 }
@@ -70,10 +76,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Here are The Top Three Best Armor Pieces for Titans:
+      Here are The Top Three Best Armor Pieces for Titans:
        <TopList />
+       <div className="columns">
        <Information />
        <Decision />
+       </div>
       </header>
     </div>
   );
